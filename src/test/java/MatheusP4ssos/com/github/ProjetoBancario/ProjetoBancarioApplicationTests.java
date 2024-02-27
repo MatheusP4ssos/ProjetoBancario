e1d758dc7935e7aa2ab2f00c2bef3045e3fabacf
Matheus Passos <matheus.senai2016@gmail.com>
2024-02-26 21:15:43 -0300

    first commit

---
+++ src/test/java/MatheusP4ssos/com/github/ProjetoBancario/ProjetoBancarioApplicationTests.java
@@ -0,0 +1,13 @@
+package MatheusP4ssos.com.github.ProjetoBancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBancarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
