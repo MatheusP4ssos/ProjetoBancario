897186ae0f0a8863d63bbcffbc1200a15ccaa74e
Matheus Passos <matheus.senai2016@gmail.com>
2024-02-26 22:58:28 -0300

    aster

---
+++ src/main/java/MatheusP4ssos/com/github/ProjetoBancario/Persona.java
@@ -0,0 +1,44 @@
+package MatheusP4ssos.com.github.ProjetoBancario;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+
+@Data
+@Builder
+@Entity //Isso aqui define que é uma classe do banco de dados
+@NoArgsConstructor
+@AllArgsConstructor
+public class Persona {
+
+ @Id //Define qual é a primary Key do banco de dados
+ @GeneratedValue(strategy = GenerationType.IDENTITY) //Define o tipo de gerador
+ private Long id;
+
+ @Column(name = "name", nullable = false) //Define o padrão para a coluna
+ private String name;
+
+ @Email
+ private String email;
+ private String password;
+
+ @CPF
+ private String cpf;
+
+ private LocalDate birthDay;
+ private Character gender;
+ private LocalDate whenCreated;
+ private LocalDate lastLogin;
+
+}
+
+
