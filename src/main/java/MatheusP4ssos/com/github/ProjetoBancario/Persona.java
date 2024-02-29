0874c0cd6a9d6d3dab0c34b77ca91b7315d7c3d9
Matheus Passos <matheus.senai2016@gmail.com>
2024-02-29 19:30:10 -0300

    aster

--- src/main/java/MatheusP4ssos/com/github/ProjetoBancario/Persona.java
+++ src/main/java/MatheusP4ssos/com/github/ProjetoBancario/Persona.java
@@ -13,30 +13,34 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.br.CPF;
 
-@Data
-@Builder
-@Entity //Isso aqui define que é uma classe do banco de dados
-@NoArgsConstructor
-@AllArgsConstructor
+@Data     //Gera o Get, Set, Equals e HashCode
+@Builder  //Habilita a criação de um objeto staticamente
+@Entity   //Isso aqui define que é uma classe do banco de dados
+@NoArgsConstructor  //Cria um construtor vazio
+@AllArgsConstructor //Cria um construtor com todos parametros
 public class Persona {
 
  @Id //Define qual é a primary Key do banco de dados
- @GeneratedValue(strategy = GenerationType.IDENTITY) //Define o tipo de gerador
+ @GeneratedValue(strategy = GenerationType.IDENTITY) //Define o tipo de gerador do PK
  private Long id;
 
  @Column(name = "name", nullable = false) //Define o padrão para a coluna
  private String name;
 
- @Email
+ @Email  //Faz a validação do tipo email (@ .com)
  private String email;
+
  private String password;
 
- @CPF
+ @CPF  //Faz a validação matematica do CPF
  private String cpf;
 
  private LocalDate birthDay;
+
  private Character gender;
+
  private LocalDate whenCreated;
+
  private LocalDate lastLogin;
 
 }
